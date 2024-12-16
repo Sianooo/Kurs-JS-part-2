48e809c484cc7dece49a4a42f1c591eee6665d7d
Siano <117085166+Sianooo@users.noreply.github.com>
2024-12-16 19:38:30 +0100

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,57 @@
+// let js = 'amazing';
+// if(js === 'amazing'){
+//     alert('JavaScript is amazing!');
+// }
+
+// console.log(40 + 8 + 23 - 10);
+
+// const markweight=75;
+// const markheight=1.90;
+// const johnweight=65;
+// const johnheight=1.75;
+
+// const BMI=markweight/markheight**2 ;
+// const BMI2=johnweight/johnheight**2;
+// console.log(BMI,BMI2);
+
+// if(BMI2>BMI){
+//     console.log(`JOHN's BMI (${BMI2}) is higher than mark's (${BMI})!`)
+// }else{
+//     console.log(`MARK>'s BMI (${BMI}) is higher than john's (${BMI2})!`)
+// }
+
+// const dolphinsScore=(96 + 108 + 89) / 3;
+// const koalasScore=(88 + 91 + 110) / 3;
+// console.log(dolphinsScore,koalasScore);
+
+// if(dolphinsScore>koalasScore){
+//     console.log('Dolphins win the trophy');
+// }else if(dolphinsScore<koalasScore){
+//     console.log('Koalas win the trophy');
+// }else{
+//     console.log('Both win the trophy');
+// }
+
+
+// let price=100;
+// let tip=price>=50 && price<=300 ? 0.15 : 0.2;
+// console.log(`The bill was ${price}, the tip was ${price*tip}, and the total value ${price*(1+tip)}`);
+
+// const age =19;
+// const isOlder = age >= 18;
+
+// if(isOlder){
+//     console.log('You can drive a car, ypu can get a license');
+// }else{
+//     console.log("You cant drive a car");
+// }
+
+// const bill = 430;
+
+// /* Write your code below. Good luck! 🙂 */
+// if(bill >=50 && bill <=300){
+//     console.log(`The bill was ${bill}, the tip was ${bill*0.15}, and the total value ${bill+bill*0.15}`)
+// }else{
+//     console.log(`The bill was ${bill}, the tip was ${bill*0.2}, and the total value ${bill+bill*0.2}`)
+// }
+
---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,27 @@
+// Remember, we're gonna use strict mode in all scripts now!
+// "use strict";
+
+// const x = 23;
+// if (x == 23) console.log(23);
+
+// const measuerKelvin=function(){
+//     const measurement={
+//         type:'temp',
+//         unit:'celsius',
+//         value:parseInt(prompt('Degrees celsius'))
+//     }
+
+//     const kelvin=measurement.value+273;
+//     return kelvin
+// }
+// console.log(measuerKelvin());
+
+// let array=[17,21,23];
+// let array=[12,5,-5,0,4]
+// const printForecast = (array) => {
+//     for(let i=0;i<array.length;i++){
+//         console.log(`${array[i]}°C in ${i+1} days`);
+//     }
+// }
+// printForecast(array);
+
